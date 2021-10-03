f7f97fa5a06eae46c8cd68cd06ba5ff7b189b3ad
Abhishek Maity <abhishek.maity@flutura.com>
2021-10-03 18:45:34 +0530

    updated codes

--- BubbleSort.py
+++ BubbleSort.py
@@ -3,13 +3,13 @@ def bubble_sort(elements):
 
     for k in range(size-1):
         for i in range(size-1-k):
-            if elements[i] > elements[i+1] :
+            if elements[i] > elements[i+1]:
                 temp = elements[i]
                 elements[i] = elements[i + 1]
                 elements[i + 1] = temp
 
 
-if __name__ == '__main__' :
+if __name__ == '__main__':
     elements = [5,9,2,1,67,34,88,34]
     bubble_sort(elements)
     print(elements)
